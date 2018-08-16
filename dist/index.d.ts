761b805688f6b325ece521be4bf30f4dcf91a774
Sam Bleckley <sam@climatum.com>
2018-08-16 12:58:33 -0400

    Use dist only for publishing

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { fastMove, perimeterScroller, Scroller } from './utils';
-export { DragContext } from './drag-context';
-export { DragActor } from './drag-actor';
-export { Draggable } from './draggable';
